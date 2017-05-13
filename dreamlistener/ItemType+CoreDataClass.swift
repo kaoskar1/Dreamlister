4993016c5dd52588299eccc062409b4bbcdcd736
oskar <oscar.ljungdahl@hotmail.com>
2017-05-13 19:26:06 +0200

    created entis swift files

---
+++ dreamlistener/ItemType+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ItemType+CoreDataClass.swift
+//  dreamlistener
+//
+//  Created by oscar ljungdahl on 2017-05-13.
+//  Copyright © 2017 oskar ljungdahl. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class ItemType: NSManagedObject {
+
+}
