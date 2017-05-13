04bfd43dfc91ed288d7d3ce141dc1993cae58c14
oskar <oscar.ljungdahl@hotmail.com>
2017-05-13 20:52:42 +0200

    tableview, materielview

---
+++ dreamlistener/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  dreamlistener
+//
+//  Created by oscar ljungdahl on 2017-05-13.
+//  Copyright © 2017 oskar ljungdahl. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+  
+
+}
--- dreamlistener/MainVC.swift
+++ dreamlistener/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  dreamlistener
 //
 //  Created by oscar ljungdahl on 2017-05-13.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
